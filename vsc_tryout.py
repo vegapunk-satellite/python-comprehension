6d579376b4164f2424d81aa866d012420c79a0ba
vegapunk-stella <arca.arikan@gmail.com>
2023-07-10 22:47:32 +0300

    Removed some unnecessary code

--- vsc_tryout.py
+++ vsc_tryout.py
@@ -1,17 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-    greeting = "Hello, {}!".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://xkcd.com/353/")
 print(r.status_code)
+print(r.ok)
