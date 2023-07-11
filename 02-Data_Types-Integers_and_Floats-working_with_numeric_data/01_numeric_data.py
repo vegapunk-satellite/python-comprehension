570e594dc5472757f4f4171b65940a44ea09b122
vegapunk-stella <arca.arikan@gmail.com>
2023-07-12 00:31:21 +0300

    Initial commit

---
+++ 02-Data_Types-Integers_and_Floats-working_with_numeric_data/01_numeric_data.py
@@ -0,0 +1,19 @@
+# Passing in variables which contain numeric data to the 'type()' function allows user grab data type.
+num = 3
+num_pi = 3.14
+
+print(type(num))
+print(type(num_pi))
+
+
+# Arithmetic Operators:
+# Addition              +
+# Subtraction           -
+# Multiplication        *
+# Division              /
+# Floor Division        //
+# Exponent              **
+# Modulus               %
+
+# While I'm sure everybody reading the script is pretty familiar with first four operators;
+# If users wish to drop the decimal on division operation, using floor division will suffice.
--- CompSci.py
+++ CompSci.py
@@ -1,14 +1,4 @@
 
-#-------------------------------------------------------------------------------------
-##Arithmetic Operators: # $ #
-##Addition:             3 + 2
-##Subtraction:          3 - 2
-##Multiplication:       3 * 2
-##Division:             3 / 2
-##Floor Division:       3 // 2
-##Exponent:             3 ** 2
-##Modulus:              3 % 2
-
 num = 1
 num *= 10
 
