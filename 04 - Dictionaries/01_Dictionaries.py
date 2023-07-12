1237ed8fef6aa943239bd38b11636c44ee688b6a
vegapunk-stella <arca.arikan@gmail.com>
2023-07-12 22:31:51 +0300

    done editing until conditionals

---
+++ 04 - Dictionaries/01_Dictionaries.py
@@ -0,0 +1,53 @@
+# Dictionaries allow users to work with key & value pairs.
+# Users may have heard this concept from other languages; under names of 'hash maps' or 'associative arrays'.
+# Key & value pairs are two linked values; where the 'key' is a unique indentifier, that lets us find the data. And the 'value' is that data.
+
+# Let us try defining a student variable using a dictionary:
+student = {
+    "name": "Robin",
+    "age": 30,
+    "courses": ["History", "CompSci"],
+}  #'value/s' can be any type of immutable data.
+print(student["name"])
+print(student["age"])
+print(student["courses"])
+
+# If users search an invalid key, python will prompt KeyError:
+# print(student['phone'])
+
+# We can overcome KeyError issue by simply using '.get()' method.
+# Instead of the error message, it will return None.
+print(student.get("phone"))  # Returns None
+print(student.get("name"))  # Returns the value of the  specific key
+
+# We can also specify a default value for the keys that don't exist;
+# by passing that default value as a second argument;
+print(student.get("phone", "Not Found"))
+
+# Adding that non-existed key into the dictionary:
+# dict['key'] = value
+student["phone"] = "556-8787"
+# Updating the value of an existed list:
+student["name"] = "Nico Robin"
+print(student)
+
+# Updating the whole dictionary:
+# Update method allows user to have more configuration over the specific dictionary.
+student.update(
+    {
+        "name": "Tony Chopper",
+        "age": 17,
+        "phone": "556-0110",
+        "courses": ["Medicine", "Chemistry"],
+    }
+)
+print(student)
+
+# Removing values from the dictionary:
+# Removing the key directly:
+del student["phone"]
+
+# Popping the key to store it inside a variable:
+age = student.pop("age")
+print(student)
+print(age)
---
+++ 04 - Dictionaries/02_Looping_through_dictionaries.py
@@ -0,0 +1,14 @@
+student = {"name": "Sanji", "age": 21, "courses": ["Gastronomy", "Art"]}
+
+print(len(student))  # Returns the number of keys inside that dictionary.
+print(student.keys())  # Returns all the specific key names.
+print(student.values())  # Returns all the values.
+print(student.items())  # Returns key and value pairs.
+
+# Looping through key and values:
+for x in student:
+    print(x)  # Standard looping will only print keys, not the values.
+
+# '.items()' method we previously saw let's users loop through key & value pairs.
+for key, value in student.items():
+    print(key, value)  # Proper syntax for printing out both keys and values.
