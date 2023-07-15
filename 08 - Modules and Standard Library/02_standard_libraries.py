2cacd4d88a5823a94ac7051317906f41c857a50b
vegapunk-stella <arca.arikan@gmail.com>
2023-07-15 19:39:40 +0300

    Added in the necessary explanations up until the file objections.

--- 08 - Modules and Standard Library/02_standard_libraries.py
+++ 08 - Modules and Standard Library/02_standard_libraries.py
@@ -1,59 +1,67 @@
 # Standard Library is incredibly useful when performing a common task; which most likely someone has already written.
-# These scripts have been optimised to be as performant as possible. and has been written by professional coders.
+# These scripts have been optimised to be as performant as possible, and have been written by professional coders.
 # Allows users import directly within Standard Libraries; without installing anything separately.
 
 
+# Random Module:
+# Grabbing a random value; within a list of values:
 import random
 
 
 courses = ["History", "Math", "Physics", "CompSci"]
 
 
-random_course = random.choice(courses)
+random_course = random.choice(
+    courses
+)  # Whenever users pass in the given list as an argument inside the '.choice()' method, and set that value as a new variable.
 
 
-print(random_course)
+print(
+    random_course
+)  # Prints out a random item from the user list each time it's executed.
 
 
+# Math Module:
+# In cases where users need to perform common mathematical operations:
+# Let us try out the 'math' module with some trigonometric functions.
 import math
 
 
-courses = ["History", "Math", "Physics", "CompSci"]
-
-
-rads = math.radians(90)
+rads = math.radians(90)  # Converting 90 degrees into radians.
 
 
 print(rads)
-print(math.sin(rads))  # 'sin' --Sine is one of the trigonometric functions--
+print(math.sin(rads))  # Returns the sine of 90 degrees.
 
 
+# Datetime Module allows users to work with days and times.
 import datetime
 import calendar
 
 
-courses = ["History", "Math", "Physics", "CompSci"]
-
-
-today = datetime.date.today()
+today = datetime.date.today()  # Returns the current date.
 print(today)
 
 
-print(calendar.isleap(2020))
+print(
+    calendar.isleap(2020)
+)  # Returns 'True' if the argument is indeed a leap year, returns 'False' if not.
 
 
+# OS Module:
+# Tends to be useful in cases where users need to access the underlying operating system.
+# OS Module has tons of functionality like; scanning the filesystem, creating files, deleting files...
 import os
-
-
-courses = ["History", "Math", "Physics", "CompSci"]
 
 
 print(
     os.getcwd()
-)  #'getcwd' function prints out the current working directory where the script is located
-print(
-    os.__file__
-)  # '__file__' function prints out the Standard Library file's location in the directory
+)  # Returns the current working directory; where the script is located.
+
+
+# Discovering through the files and scripts of the Standard Library will be beneficial for the users:
+# Since all these useful methods and their contents can be found within.
+print(os.__file__)  # Returns the Standard Library directory.
 
 
 # A standard library which leads the browser to open a webcomic.
---
+++ 08 - Modules and Standard Library/03_os_module.py
@@ -0,0 +1,126 @@
+# OS Module helps users access the underlying operating system.
+import os
+
+
+print(
+    dir(os)
+)  # Prints out all of the attributes and methods we can access to within this module.
+
+
+# Commonly used OS Module Functionalities:
+# Accessing the current working directory:
+print(os.getcwd())  # Prints out the directory of the current script.
+
+
+# Navigating into a lew location on the filesystem:
+os.chdir(
+    "C:/Users/MSI/Desktop/Vega Records/my_modules"
+)  # Changes the current directory to the desired new location.
+
+
+# Accessing all the files and folders on current working directory:
+print(os.listdir())  # Prints out the files & folders in the current directory.
+
+
+# Creating folders and directories:
+os.mkdir("hsdh-OP")  # creates a single folder in the current directory.
+os.makedirs("hsdh-OP/hito")  # creates multiple folders in the current directory.
+
+
+print(os.listdir())
+
+
+# Deleting folders and directories:
+os.rmdir("hsdh-OP")  # deletes a single folder on the current directory
+os.removedirs("hsdh-OP/hito")  # deletes multiple folders on the current directory
+
+
+print(os.listdir())
+
+
+# Renaming files and folders:
+# Proper syntax;
+# os.rename("file_to_be_renamed", "new_name")
+os.rename("importing_modules", "importing_modules.py")
+print(os.listdir())
+
+
+# Obtaining information about files:
+print(
+    os.stat("importing_modules.py")
+)  # Returns various file information that later can be used.
+print(os.stat("importing_modules.py").st_size)  # Returns the size of that file.
+print(
+    os.stat("importing_modules.py").st_mtime
+)  # Returns the last modification time of that file, in the form of a 'timestamp'.
+
+
+# Converting the returned timestamp; into a more user friendly and readable format.
+import os
+from datetime import datetime
+
+
+print(os.getcwd())
+mod_time = os.stat(
+    "importing_modules.py"
+).st_mtime  # Setting the returned 'timestamp' value as a variable.
+print(
+    datetime.fromtimestamp(mod_time)
+)  # And passing in that variable as an argument for '.fromtimestamp()'. Prompts the readable date.
+
+
+# Traversing the entire directory tree and files within it:
+# '.walk()' method is a generator that yields a tuple of three values as it walks the directory tree.
+# For each directory that it sees; yields the directory path, directories within that path and files within that directories.
+# By default traverses from top going down.
+import os
+
+
+for dirpath, dirnames, filenames in os.walk(
+    os.getcwd()
+):  # If the desired directory is not the current one we can pass in a specific directory to our argument.
+    print("Current Path:", dirpath)
+    print("Directories:", dirnames)
+    print("Files:", filenames)
+# 'os.walk' method useful in cases like finding lost files or keeping track of the file information of a web application; within a certain directory structure.
+
+
+# Users can access their home directory location by grabbing home environment variable:
+import os
+
+
+print(os.getcwd())
+# print(os.environ) # Prompts out all created environments.
+print(os.environ.get("HOMEPATH"))  # Prompts out only the home directory.
+
+
+# Combining a file with the desired path:
+# Could be done using concatenation but leaves room for user mistakes, so instead;
+# 'os.path.join()' method tends to be a lot more convenient
+file_path = os.path.join(os.environ.get("HOMEPATH"), "test.txt")
+print(file_path)
+
+
+# Various convenient methods available through using 'os.path':
+print(
+    os.path.basename("/tmp/test.txt")
+)  # Returns the file name, and does not have to be a real path.
+print(os.path.dirname("/tmp/test.txt"))  # Returns the directory name.
+print(os.path.split("/tmp/test.txt"))  # Returns both file and directory names.
+print(
+    os.path.exists("/Users/MSI")
+)  # Returns true if a path exists within the filesystem, false if not.
+print(
+    os.path.isdir("/Users/MSI")
+)  # Returns true if a specific path is directory, false if not.
+print(
+    os.path.isfile("/Users/MSI")
+)  # Returns true if a specific path is file, false if not.
+print(
+    os.path.splitext("/tmp/test.txt")
+)  # Splits the root of the path and its extension.
+
+
+print(
+    dir(os.path)
+)  # Users can see all the useful functions and attributes this method has within.
